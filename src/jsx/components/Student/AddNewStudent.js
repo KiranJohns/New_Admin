47ee7a070510ee21feff4f3f11a47b2565ad8f6f
rahul <rahulor2000@gmail.com>
2023-11-15 12:06:02 +0530

    create user route updated

--- src/jsx/components/Student/AddNewStudent.js
+++ src/jsx/components/Student/AddNewStudent.js
@@ -1,106 +1,209 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import DatePicker from "react-datepicker";
-import { IMAGES } from '../Dashboard/Content';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
-import { Row, Col, Card, Button, Dropdown, ButtonGroup, Tab, Nav } from "react-bootstrap";
+import { IMAGES } from "../Dashboard/Content";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import swal from "sweetalert";
+import {
+  Row,
+  Col,
+  Card,
+  Button,
+  Dropdown,
+  ButtonGroup,
+  Tab,
+  Nav,
+} from "react-bootstrap";
+import fetchData from "../../../axios";
 
 const AddNewStudent = () => {
-    const [file, setFile] = useState(null)
-    const fileHandler = (e) => {
-        setFile(e.target.files[0]);
-    }
-    const RemoveFile = () => {
-        setFile(null)
-    }
+  const makeRequest = fetchData();
+  const [userData, setUserData] = useState({
+    email: "",
+    first_name: "",
+    last_name: "",
+    password: "",
+    phone: "",
+    country: "",
+    city: "",
+  });
+  function handleOnchange(e) {
+    setUserData((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
 
-    const [startDate, setStartDate] = useState(new Date());
-    const onChange = (date) => {
-        setStartDate(date);
-    };
-    return (
-        <>
-            <div className="row">
-                <div className="col-xl-10">
-                    <div className="card">
-                        <div className="card-header">
-                            <h5 className="mb-0">User Details</h5>
-                        </div>
-                        <div className="card-body">
-                            <div className="row">
-                                {/* <div className="col-xl-3 col-lg-4">
-                                    <label className="form-label text-primary">Photo<span className="required">*</span></label>
-                                        <div className="avatar-upload">
-                                        <div className="avatar-preview">
-                                            <div id="imagePreview"                                                 
-                                                style={{backgroundImage: file ? "url(" + URL.createObjectURL(file) + ")" : "url(" + IMAGES.noimage +")" }}
-                                            > 			
-                                            </div>
-                                        </div>
-                                        <div className="change-btn mt-2 mb-lg-0 mb-3">
-                                            <input type='file' className="form-control d-none"  onChange={fileHandler}  id="imageUpload" accept=".png, .jpg, .jpeg" />
-                                            <label htmlFor="imageUpload" className="dlab-upload mb-0 btn btn-primary btn-sm">Choose File</label>
-                                            <Link to={"#"} className="btn btn-danger light remove-img ms-2 btn-sm" onClick={RemoveFile}>Remove</Link>
-                                        </div>
-                                    </div>	
-                                </div> */}
-                                <div className="col-xl-9 col-lg-8">
-                                    <div className="row">
-                                        <div className="col-xl-6 col-sm-6">
-                                            <div className="mb-3">
-                                                <label htmlFor="exampleFormControlInput1" className="form-label text-primary">First Name<span className="required">*</span></label>
-                                                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="First Name" />
-                                            </div>
-                                            <div className="mb-3">
-                                                <label htmlFor="exampleFormControlInput1" className="form-label text-primary">Email<span className="required">*</span></label>
-                                                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="First Name" />
-                                            </div>
-                                            <div className="mb-3">
-                                                <div className="form-group mb-3">
-                                                    <label htmlFor="exampleFormControlInput1" className="form-label text-primary">Country<span className="required">*</span></label>
-                                                    <select
-                                                        defaultValue={"option"}
-                                                        className="form-control "
-                                                    >
-                                                        <option>Select</option>
-                                                        <option>United Kingdom</option>
+  function submit(e) {
+    e.preventDefault();
+    makeRequest("POST","/info/create-user",userData).then(res => {
+        swal("Done!","user successfully created","success")
+    }).catch(err => {
+        swal("Oops!",err.data.errors[0].error,"error")
+        console.log(err);
+    })
+    console.log(userData);
+  }
 
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div className="mb-3">
-                                                <label htmlFor="pas" className="form-label text-primary">Password<span className="required">*</span></label>
-                                                <input type="text" className="form-control" id="exampleFormControlInput4" placeholder="Wally" />
-                                            </div>
-                                        </div>
-                                        <div className="col-xl-6 col-sm-6">
-                                            <div className="mb-3">
-                                                <label htmlFor="exampleFormControlInput4" className="form-label text-primary">Phone<span className="required">*</span></label>
-                                                <input type="text" className="form-control" id="exampleFormControlInput4" placeholder="Wally" />
-                                            </div>
-                                            <div className="mb-3">
-                                                <label htmlFor="exampleFormControlInput5" className="form-label text-primary">Email<span className="required">*</span></label>
-                                                <input type="text" className="form-control" id="exampleFormControlInput5" placeholder="Mana William" />
-                                            </div>
-                                            <div className="mb-3">
-                                                <label htmlFor="exampleFormControlInput6" className="form-label text-primary">City<span className="required">*</span></label>
-                                                <input type="number" className="form-control" id="exampleFormControlInput6" placeholder="+123456789" />
-                                            </div>
-                                            <div className="mb-3 d-flex justify-content-center mt-5 ml-4">
-                                                <Button className="" variant="primary">
-                                                    Submit
-                                                </Button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="row">
+        <div className="col-xl-10">
+          <div className="card">
+            <div className="card-header">
+              <h5 className="mb-0">User Details</h5>
             </div>
-            {/* <div className="col-xl-12">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-xl-9 col-lg-8">
+                  <div className="row">
+                    <div className="col-xl-6 col-sm-6">
+                      <div className="mb-3">
+                        <label
+                          htmlFor="exampleFormControlInput1"
+                          className="form-label text-primary"
+                        >
+                          First Name<span className="required">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          name="first_name"
+                          value={userData.first_name}
+                          onChange={handleOnchange}
+                          className="form-control"
+                          id="exampleFormControlInput1"
+                          placeholder="First Name"
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label
+                          htmlFor="exampleFormControlInput1"
+                          className="form-label text-primary"
+                        >
+                          Email<span className="required">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          name="email"
+                          value={userData.email}
+                          onChange={handleOnchange}
+                          className="form-control"
+                          id="exampleFormControlInput1"
+                          placeholder="Email"
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <div className="form-group mb-3">
+                          <label
+                            htmlFor="exampleFormControlInput1"
+                            className="form-label text-primary"
+                          >
+                            Country<span className="required">*</span>
+                          </label>
+                          <select
+                          name="country"
+                            onChange={handleOnchange}
+                            className="form-control "
+                          >
+                            <option>Select</option>
+                            <option value="United Kingdom">
+                              United Kingdom
+                            </option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="mb-3">
+                        <label
+                          htmlFor="pas"
+                          className="form-label text-primary"
+                        >
+                          Password<span className="required">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          name="password"
+                          value={userData.password}
+                          onChange={handleOnchange}
+                          className="form-control"
+                          id="exampleFormControlInput4"
+                          placeholder="Password"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-xl-6 col-sm-6">
+                      <div className="mb-3">
+                        <label
+                          htmlFor="exampleFormControlInput5"
+                          className="form-label text-primary"
+                        >
+                          Last Name<span className="required">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="exampleFormControlInput5"
+                          placeholder="Last Name"
+                          name="last_name"
+                          value={userData.last_name}
+                          onChange={handleOnchange}
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label
+                          htmlFor="exampleFormControlInput4"
+                          className="form-label text-primary"
+                        >
+                          Phone<span className="required">*</span>
+                        </label>
+                        <input
+                          type="number"
+                          name="phone"
+                          value={userData.phone}
+                          onChange={handleOnchange}
+                          className="form-control"
+                          id="exampleFormControlInput4"
+                          placeholder="Phone"
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label
+                          htmlFor="exampleFormControlInput6"
+                          className="form-label text-primary"
+                        >
+                          City<span className="required">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          name="city"
+                          value={userData.city}
+                          onChange={handleOnchange}
+                          className="form-control"
+                          id="exampleFormControlInput6"
+                          placeholder="City"
+                        />
+                      </div>
+                      <div className="mb-3 d-flex justify-content-center mt-5 ml-4">
+                        <Button
+                          className=""
+                          variant="primary"
+                          type="button"
+                          onClick={submit}
+                        >
+                          Submit
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="col-xl-12">
                 <div className="card">
                     <div className="card-header">
                         <h5 className="mb-0">Parents Details</h5>
@@ -150,8 +253,8 @@ const AddNewStudent = () => {
                     </div>
                 </div>
             </div> */}
-        </>
-    );
+    </>
+  );
 };
 
 export default AddNewStudent;
