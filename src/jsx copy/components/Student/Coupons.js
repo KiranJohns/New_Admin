f88f277ca6b0f057f9ddbe6bd83ce9c4de32afeb
rahul <rahulor2000@gmail.com>
2023-12-21 23:50:05 +0530

    change orfer of coupon list

--- src/jsx copy/components/Student/Coupons.js
+++ src/jsx copy/components/Student/Coupons.js
@@ -61,8 +61,8 @@ const CouponList = () => {
     .then((res) => {
       if (res?.data.response) {
         console.log(res?.data.response);
-        setCoupons(res?.data.response);
-        setAllCoupons(res?.data.response);
+        setCoupons(res?.data.response.reverse());
+        setAllCoupons(res?.data.response.reverse());
       }
     })
     .catch((err) => {
